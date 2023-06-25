91c6bafdfafc9d5b8e2e70b2dc6f0ad7008be91f
Solasa Venkata Naga Ramesh <82230347+rameshsolasa17@users.noreply.github.com>
2023-06-25 11:34:39 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.87db5da53fbc34584b174e74562ee034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d75b608b7ba5c071508b50c20c82cfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "076bdf135e5271638daf",
+    "url": "/static/css/main.010fcd9a.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "076bdf135e5271638daf",
+    "url": "/static/js/main.e0a34135.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
