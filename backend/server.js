12120f0b8b0476461a16857ae6e7e515681d6ec0
Ramesh Solasa <rameshsolasa17@gmail.com>
2025-07-01 12:30:33 -0500

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,14 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json())
-app.use(cors());
+app.use(cors())
+
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+  
+    next();
+})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
